b4f81b0e05f8d94887021a2f001824f108899910
javiermilesinacap <109394677+javiermilesinacap@users.noreply.github.com>
2022-10-20 13:22:20 -0300

    movimiento juego
    Falta validar escenarios

--- flota1310.py
+++ flota1310.py
@@ -10,7 +10,7 @@ class Vehiculo:
     def retroceder(self):
         self.lat -= (self.velocidad/100)+5
         pass
-    def __init__(self,velocidad=0,lat=0.0,lon=0.0,modelo="",ppu="",color="") -> None:
+    def __init__(self,velocidad=0,lat=0.0,lon=300.0,modelo="",ppu="",color="") -> None:
         self.velocidad = velocidad
         self.lat = lat
         self.lon = lon
@@ -19,19 +19,34 @@ class Vehiculo:
         self.color= color
     def __str__(self)->str:
         return (f'El vehiculo {self.modelo} patente {self.ppu} color {self.color} va a {self.velocidad} km/h')
+class Carretera: 
+    ejex = 0.0
+    ejey = 0.0
+carretera = Carretera()
 objeto = Vehiculo()
 pygame.init()
-ventana = pygame.display.set_mode((800,600))
+ventana = pygame.display.set_mode((1024,900))
 autito = pygame.image.load('./car.png').convert_alpha()
+fondo = pygame.image.load('./fondo.png').convert_alpha()
 while(True):
     for event in pygame.event.get():
-        if(event.type== pygame.KEYDOWN and event.key == pygame.K_w):
+        if(event.type== pygame.KEYDOWN and event.key == pygame.K_d):
             objeto.acelerar()
             objeto.avanzar()
             print(objeto)    
-        if(event.type== pygame.KEYDOWN and event.key == pygame.K_s):
+        if(event.type== pygame.KEYDOWN and event.key == pygame.K_a):
             objeto.frenar()
             objeto.retroceder() 
-            print(objeto)  
-        ventana.blit(autito,(objeto.lat,300))
-        pygame.display.update() 
+            print(objeto)
+        if(event.type== pygame.KEYDOWN and event.key == pygame.K_s):
+            objeto.lon +=10
+            pass   
+        if(event.type== pygame.KEYDOWN and event.key == pygame.K_w):
+            objeto.lon -= 10
+            pass
+    carretera.ejex -= objeto.velocidad/100
+    if(carretera.ejex<-500):
+        carretera.ejex = 0
+    ventana.blit(fondo,(carretera.ejex,carretera.ejey))
+    ventana.blit(autito,(objeto.lat,objeto.lon))
+    pygame.display.update() 
